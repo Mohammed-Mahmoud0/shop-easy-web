8fdb5b6a53e38b8de2db45df9386429ae4bbfd39
ITI197 <ITI197@isi.net>
2025-09-23 13:38:23 +0300

    ui of products done

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
     searchBar.classList.toggle("active");
   });
 });
+
+async function fetchData() {
+  const response = await fetch("https://fakestoreapi.com/products/1");
+  const data = await response.json();
+  return data;
+}
+
+async function main() {
+  data = await fetchData();
+  console.log(data["category"]);
+}
+
+main();
